ddac44c1a11fbefc5237cd46bc05987366121d22
varun <varungoyal311213@gmail.com>
2020-09-14 08:04:36 +0530

    Auto-Commit at : Mon Sep 14 08:04:35 IST 2020

--- watch.sh
+++ watch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-path="/home/varun/WORK/HUMACi\ Internship\ all/Nextmeet\ Flask\ Test\ Project" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
+path="/home/varun/WORK/HUMACi Internship all/Nextmeet Flask Test Project" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
 while true 
 do
-	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
+	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write "$path"
 	cd $path                              > /dev/null &> /dev/null
 	git add --all                         > /dev/null &> /dev/null
 	now=$(date)                           > /dev/null &> /dev/null
